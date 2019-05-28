6dba9f86e2e4af08e88e4ff4a04d06de32208bd2
Bernardo Caputo <bernardocaputo@gmail.com>
2019-05-28 20:58:55 -0300

    account test

---
+++ test/support/graphql_helper.ex
@@ -0,0 +1,18 @@
+defmodule Bank.GraphqlHelper do
+  use Phoenix.ConnTest
+
+  @endpoint BankWeb.Endpoint
+
+  def graphql_query(conn, options) do
+    conn
+    |> post("/graphql", build_query(options[:query], options[:variables]))
+    |> json_response(200)
+  end
+
+  defp build_query(query, variables) do
+    %{
+      "query" => query,
+      "variables" => variables
+    }
+  end
+end
