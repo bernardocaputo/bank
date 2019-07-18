6af23079451cb4a6045c583e1ae1e43ef02aba2f
Bernardo Caputo <bernardocaputo@gmail.com>
2019-07-17 23:05:11 -0300

    set two different Repo configuration

--- config/dev.exs
+++ config/dev.exs
@@ -36,11 +36,20 @@ config :logger, :console, format: "[$level] $message\n"
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
 
-# Configure your database
+# docker dev
+# config :bank, Bank.Repo,
+#   adapter: Ecto.Adapters.Postgres,
+#   username: System.get_env("DB_USERNAME"),
+#   password: System.get_env("DB_PASSWORD"),
+#   database: System.get_env("DB_NAME"),
+#   hostname: "postgres",
+#   pool_size: 10
+
+# dev
 config :bank, Bank.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("DB_USERNAME"),
-  password: System.get_env("DB_PASSWORD"),
-  database: System.get_env("DB_NAME"),
-  hostname: "postgres",
+  username: "postgres",
+  password: "postgres",
+  database: "bank_dev",
+  hostname: "localhost",
   pool_size: 10
