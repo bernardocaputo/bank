c23486e3167bd3f236e022fdc634434bca60aeff
Bernardo Caputo <bernardocaputo@gmail.com>
2020-08-12 21:16:24 -0300

    clear cache

--- test/support/authentication_helper.ex
+++
@@ -1,8 +0,0 @@
-defmodule Bank.AuthenticationHelper do
-  def authenticate_user(conn, user) do
-    {:ok, token, _} = Guardian.encode_and_sign(user)
-
-    conn
-    |> Plug.Conn.put_req_header("authorization", "Bearer #{token}")
-  end
-end
